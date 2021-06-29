367e53990cae3e623bf9a9b2a62db0dffbb0f659
Chamano Ramashia <chamano.sr@gmail.com>
2021-06-29 13:57:41 +0200

    adding mock articles

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,31 @@
+import { Article } from "./article";
+
+export const ARTICLES: Article[] = [
+    {
+        id: 1,
+        title: "My fisrt article",
+        key: "my-first-article",
+        date: new Date(),
+        content: "",
+        description: "This is my forst article, its great please read it",
+        imageUrl: "../assets/images/about.jpg",
+    },
+    {
+        id: 2,
+        title: "the second article",
+        key: "the-second-article",
+        date: new Date(),
+        content: "",
+        description: "This is my forst article, its great please read it",
+        imageUrl: "../assets/images/acc.jpg",
+    },
+    {
+        id: 3,
+        title: "My third writing",
+        key: "my-third-writing",
+        date: new Date(),
+        content: "",
+        description: "This is my forst article, its great please read it",
+        imageUrl: "../assets/images/blog.jpg",
+    },
+]
