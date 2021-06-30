42af37e95c227966196d5ec1ce1a1913e4ba4edc
Chamano Ramashia <chamano.sr@gmail.com>
2021-06-30 09:59:34 +0200

    adding 404 page

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -15,4 +15,9 @@ export class ArticleService {
     const articles: Article[] = ARTICLES;
     return of(articles)
   }
+
+  getArticle(key: String): Observable<Article>{
+      const articles: Article[] = ARTICLES.filter(a => a.key === key)
+      return of(articles[0])
+  }
 }
