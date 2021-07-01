7b307c627911ed0c4da59e654d303faa2c48be57
Chamano Ramashia <chamano.sr@gmail.com>
2021-07-01 12:19:18 +0200

    adding meta tags

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Article } from '../article';
 import { ArticleService } from '../article.service';
@@ -19,7 +19,8 @@ export class ArticleComponent implements OnInit {
     private articleService: ArticleService,
     private router: Router,
     private titleService: Title,
-    private sharedService: SharedService) { }
+    private sharedService: SharedService,
+    private meta: Meta) { }
 
   ngOnInit(){
     this.route.params.subscribe(params =>{
@@ -31,7 +32,23 @@ export class ArticleComponent implements OnInit {
             }
             this.article = article;
             this.titleService.setTitle(`${this.article.title} - ${this.sharedService.blogTitle}`)
-
+            this.meta.addTags([
+            { name: 'description',
+              content: this.article.description
+            },
+            { property: 'og:title',
+              content: `${this.article.title} - ${this.sharedService.blogTitle}`
+            },
+            { property: 'og:type',
+              content: 'website'
+            },
+            { property: 'og:url',
+              content: this.sharedService.baseUrl + this.article.key
+            },
+            { property: 'og:image',
+              content: this.article.imageUrl
+            },
+            ]);
       } )
     })
   }
